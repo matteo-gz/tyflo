3f436952eee23f1edcde26f18707a357f77c8744
matteo <matteo-gz@users.noreply.github.com>
2024-03-05 23:07:18 +0800

    code

---
+++ cmd/socks5_server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"github.com/matteo-gz/tyflo/pkg/logger"
+	"github.com/matteo-gz/tyflo/pkg/protocol/socks5"
+	"log"
+)
+
+func main() {
+	l := logger.NewDefaultLogger()
+	ss := socks5.NewServer(l, socks5.DefaultDialer{})
+	err := ss.Start(context.Background(), ":1079")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("ok")
+	ch := make(chan struct{})
+	<-ch
+}
