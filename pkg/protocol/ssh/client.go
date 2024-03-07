e009c90f047f73d955ac1d789033eb0aeda50073
matteo <matteo-gz@users.noreply.github.com>
2024-03-07 23:34:28 +0800

    ssh recovery

--- pkg/protocol/ssh/client.go
+++ pkg/protocol/ssh/client.go
@@ -6,10 +6,13 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 type Client struct {
-	c *ssh.Client
+	c    *ssh.Client
+	addr string
+	conf *ssh.ClientConfig
 }
 
 func NewClient(file, addr, user string) (c *Client, err error) {
@@ -25,20 +28,54 @@ func NewClient(file, addr, user string) (c *Client, err error) {
 	if err != nil {
 		return
 	}
-	sshClient, err := ssh.Dial("tcp", addr, &ssh.ClientConfig{
-		User: user,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(sig),
+	c = &Client{
+		addr: addr,
+		conf: &ssh.ClientConfig{
+			User: user,
+			Auth: []ssh.AuthMethod{
+				ssh.PublicKeys(sig),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	})
+	}
+	err = c.connect()
 	if err != nil {
 		return nil, err
 	}
-	c = &Client{
-		c: sshClient,
-	}
+	go c.retry()
 	return
+}
+func (c *Client) connect() error {
+	sshClient, err := ssh.Dial("tcp", c.addr, c.conf)
+	if err != nil {
+		return err
+	}
+	c.c = sshClient
+	return nil
+}
+func (c *Client) retry() {
+	ch := make(chan struct{}, 1)
+	for {
+		select {
+		case <-ch:
+			if err := c.connect(); err != nil {
+				log.Println("connect retry", err)
+				time.Sleep(10 * time.Second)
+				ch <- struct{}{}
+			} else {
+				log.Println("ssh recovery")
+			}
+		default:
+			log.Println("ssh holding")
+			err := c.c.Wait()
+			if err != nil {
+				log.Println("ssh client err", err)
+			} else {
+				log.Println("ssh client leave")
+			}
+			ch <- struct{}{}
+		}
+	}
 }
 func (c *Client) DialContext(ctx context.Context, addr string) (conn net.Conn, err error) {
 	log.Println("ssh addr", addr)
